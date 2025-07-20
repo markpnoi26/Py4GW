68b6cf00e1e239209d64eeb3438838d1afd98f6f
Mark <markpnoi26@gmail.com>
2025-07-20 23:45:48 +0100

    added perma improvements on window

--- Widgets/HeroAICustomBehaviors.py
+++ Widgets/HeroAICustomBehaviors.py
@@ -1,15 +1,111 @@
 import os
+import pathlib
 import sys
 import traceback
 
 import Py4GW  # type: ignore
 from HeroAI.cache_data import CacheData
+from Py4GW_widget_manager import get_widget_handler
+from Py4GWCoreLib import IconsFontAwesome5
+from Py4GWCoreLib import ImGui
 from Py4GWCoreLib import IniHandler
 from Py4GWCoreLib import PyImGui
 from Py4GWCoreLib import Routines
 from Py4GWCoreLib import Timer
-from Widgets.CustomBehaviors.gui.current_build import render as current_build_render
-from Py4GW_widget_manager import get_widget_handler
+from Py4GWCoreLib.GlobalCache import GLOBAL_CACHE
+from Py4GWCoreLib.Py4GWcorelib import Color
+from Py4GWCoreLib.Py4GWcorelib import Utils
+from Widgets.CustomBehaviors.primitives.custom_behavior_loader import CustomBehaviorLoader
+from Widgets.CustomBehaviors.primitives.parties.custom_behavior_shared_memory import CustomBehaviorWidgetMemoryManager
+from Widgets.CustomBehaviors.primitives.skillbars.custom_behavior_base_utility import CustomBehaviorBaseUtility
+from Widgets.CustomBehaviors.primitives.skills.custom_skill_utility_base import CustomSkillUtilityBase
+
+shared_data = CustomBehaviorWidgetMemoryManager().GetCustomBehaviorWidgetData()
+WITH_DETAIL = False
+
+
+def render_custom_behavior_gui():
+    global WITH_DETAIL
+
+    custom_combat_behavior = CustomBehaviorLoader().custom_combat_behavior
+    if PyImGui.button(f"{IconsFontAwesome5.ICON_SYNC} Search build again"):
+        CustomBehaviorLoader().refresh_custom_behavior_candidate()
+
+    if not custom_combat_behavior:
+        PyImGui.text("Current build is None.")
+        return
+
+    PyImGui.text(f"HasLoaded : {CustomBehaviorLoader()._has_loaded}")
+    PyImGui.text(f"Selected template : {custom_combat_behavior.__class__.__name__}")
+    if custom_combat_behavior:
+        PyImGui.text(f"Account state:{custom_combat_behavior.get_state()}")
+        PyImGui.text(f"Final state:{custom_combat_behavior.get_final_state()}")
+
+    if custom_combat_behavior.get_is_enabled():
+        if PyImGui.button(f"{IconsFontAwesome5.ICON_TIMES} Disable"):
+            custom_combat_behavior.disable()
+    else:
+        if PyImGui.button(f"{IconsFontAwesome5.ICON_CHECK} Enable"):
+            custom_combat_behavior.enable()
+    pass
+
+    if custom_combat_behavior and type(custom_combat_behavior).mro()[1].__name__ != CustomBehaviorBaseUtility.__name__:
+        PyImGui.separator()
+        PyImGui.text("Generic skills : ")
+        generic_behavior_build = custom_combat_behavior.get_generic_behavior_build()
+        if generic_behavior_build:
+            for skill in generic_behavior_build:  # type: ignore
+                PyImGui.text(f"bbb {skill.skill_name}")  # type: ignore
+
+    if custom_combat_behavior and type(custom_combat_behavior).mro()[1].__name__ == CustomBehaviorBaseUtility.__name__:
+        PyImGui.separator()
+        instance: CustomBehaviorBaseUtility = custom_combat_behavior  # type: ignore
+        PyImGui.text("Utility system : ")
+        PyImGui.same_line(0, -1)
+        WITH_DETAIL = PyImGui.checkbox("with detail", WITH_DETAIL)
+
+        scores: list[tuple[CustomSkillUtilityBase, float | None]] = instance.get_all_scores()
+        if PyImGui.begin_table("skill", 2, int(PyImGui.TableFlags.SizingStretchProp)):
+            PyImGui.table_setup_column("A")
+            PyImGui.table_setup_column("B")
+
+        for score in scores:
+
+            def label_generic_utility(utility: CustomSkillUtilityBase) -> str:
+                if utility.__class__.__name__ == "HeroAiUtility":
+                    return f"{IconsFontAwesome5.ICON_GAMEPAD} "
+                return ""
+
+            current_path = pathlib.Path.cwd()
+            prefix = ""
+            if "Widgets" in str(current_path):
+                prefix = "..\\"
+
+            score_text = f"score={score[1]:06.2f}" if score[1] is not None else "score = 0"
+            texture_file = prefix + GLOBAL_CACHE.Skill.ExtraData.GetTexturePath(score[0].custom_skill.skill_id)
+
+            PyImGui.table_next_row()
+            PyImGui.table_next_column()
+            ImGui.DrawTexture(texture_file, 44, 44)
+            PyImGui.table_next_column()
+
+            skill: CustomSkillUtilityBase = score[0]
+            PyImGui.text_scaled(
+                f"{label_generic_utility(skill)}{score_text}", Color(0, 255, 0, 255).to_tuple_normalized(), 1.2
+            )
+            PyImGui.text(f"{skill.custom_skill.skill_name}")
+
+            if WITH_DETAIL:
+                PyImGui.bullet_text("required ressource")
+                PyImGui.same_line(0, -1)
+                PyImGui.text_colored(f"{skill.mana_required_to_cast}", Utils.RGBToNormal(27, 126, 246, 255))
+                PyImGui.bullet_text(f"allowed in : {[x.name for x in skill.allowed_states]}")  # type: ignore
+                PyImGui.bullet_text(f"pre_check : {skill.are_common_pre_checks_valid(instance.get_final_state())}")
+
+            PyImGui.table_next_row()
+
+        PyImGui.end_table()
+
 
 # Iterate through all modules in sys.modules (already imported modules)
 # Iterate over all imported modules and reload them
@@ -74,14 +170,14 @@ def draw_widget():
             header_text = "The following prevents you from using HeroAICustomBehavior:"
             final_text = header_text
             if not is_hero_ai_enabled:
-                final_text += "\n  - HeroAI is not running "
+                final_text += "\n  - HeroAI is not running"
             PyImGui.text(final_text)
             return
 
         PyImGui.begin_tab_bar("current_build")
 
         if PyImGui.begin_tab_item("current_build"):
-            current_build_render()
+            render_custom_behavior_gui()
             PyImGui.end_tab_item()
 
         PyImGui.end_tab_bar()
