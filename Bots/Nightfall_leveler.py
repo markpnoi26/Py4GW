56bed9f79b514f331d7c3c02f744deb91a536e36
Kendor695 <kjohn9815@msn.com>
2025-10-01 10:07:54 -0400

    Update Nightfall_leveler.py

--- Bots/Nightfall_leveler.py
+++ Bots/Nightfall_leveler.py
@@ -33,11 +33,11 @@ def create_bot_routine(bot: Botting) -> None:
     jokanur_diggings_quests(bot)
     bot.States.JumpToStepName("[H]jokanur_diggings_quests_25") #trying this, if it loops forever thats great cause I need some sleep
     #def LeveledUp(bot: Botting) -> None:
-            level = GLOBAL_CACHE.Agent.GetLevel(GLOBAL_CACHE.Player.GetAgentID())
-            if level <= 10:
-                bot.States.JumpToStepName("[H]jokanur_diggings_quests_25")
-            else: 
-                bot.States.JumpToStepName("[H]EOTN Run_27")
+            #level = GLOBAL_CACHE.Agent.GetLevel(GLOBAL_CACHE.Player.GetAgentID())
+            #if level <= 10:
+                #bot.States.JumpToStepName("[H]jokanur_diggings_quests_25")
+            #else: 
+                #bot.States.JumpToStepName("[H]EOTN Run_27")
     #LeveledUp(bot)
     #EOTN_Run(bot)
     #ExitBorealStation(bot)
