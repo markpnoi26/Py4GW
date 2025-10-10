6e6d0404580cc7230c3b516310b8cb25b618fb54
Mark <markpnoi26@gmail.com>
2025-10-10 14:50:43 +0100

    removed

--- Py4GWCoreLib/botting_src/helpers_src/Upkeepers.py
+++ Py4GWCoreLib/botting_src/helpers_src/Upkeepers.py
@@ -104,7 +104,6 @@ class _Upkeepers:
                 yield from Routines.Yield.wait(500)
                 continue
             player_email = GLOBAL_CACHE.Player.GetAccountEmail()
-
             GLOBAL_CACHE.ShMem.SendMessage(
                 player_email,
                 player_email,
@@ -114,7 +113,6 @@ class _Upkeepers:
             yield from Routines.Yield.wait(500)
             while LootingRoutineActive():
                 yield from Routines.Yield.wait(100)
-                continue
             
 
 
