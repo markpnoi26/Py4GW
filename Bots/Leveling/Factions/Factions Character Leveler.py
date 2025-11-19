e7780c4c128b3b502a5624853329eddf62333f73
Wick-Divinus <capasanta.lodi-3e@icloud.com>
2025-11-19 18:20:38 +0100

    Update Factions Character Leveler.py

--- Bots/Leveling/Factions/Factions Character Leveler.py
+++ Bots/Leveling/Factions/Factions Character Leveler.py
@@ -1075,6 +1075,7 @@ def UnlockKillroy(bot: Botting):
     bot.Dialogs.AtXY(17341.00, -4796.00, 0x84)
     bot.Wait.ForMapLoad(target_map_id=703)  # killroy_map_id
     bot.Items.Equip(24897) #brass_knuckles_item_id
+    bot.Wait.ForTime(3000)
     bot.Move.XY(19290.50, -11552.23)
     bot.Wait.UntilOnOutpost()
     bot.Move.XYAndDialog(17341.00, -4796.00, 0x835A07)  # take reward
