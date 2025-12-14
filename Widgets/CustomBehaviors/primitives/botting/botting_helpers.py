8a1aad9a046ddf069ffc0d62680276966945e640
PierreGORCE <pierre.gorce@mangopay.com>
2025-12-14 14:29:38 +0100

    custom_behaviors flagging

--- Widgets/CustomBehaviors/primitives/botting/botting_helpers.py
+++ Widgets/CustomBehaviors/primitives/botting/botting_helpers.py
@@ -1,6 +1,6 @@
 from collections.abc import Callable, Generator
 import time
-from typing import Any
+from typing import Any, List
 from Py4GWCoreLib import Player, Item, ItemArray, Bags, Routines
 from Py4GWCoreLib.AgentArray import AgentArray
 from Py4GWCoreLib.GlobalCache import GLOBAL_CACHE
@@ -11,11 +11,49 @@ from Py4GWCoreLib.py4gwcorelib_src.Lootconfig_src import LootConfig
 from Py4GWCoreLib.py4gwcorelib_src.Timer import ThrottledTimer
 
 from Widgets.CustomBehaviors.primitives.bus.event_type import EventType
+from Widgets.CustomBehaviors.primitives.custom_behavior_loader import CustomBehaviorLoader
 from Widgets.CustomBehaviors.primitives.helpers import custom_behavior_helpers
+from Widgets.CustomBehaviors.primitives.skillbars import custom_behavior_base_utility
+from Widgets.CustomBehaviors.primitives.skills.custom_skill_utility_base import CustomSkillUtilityBase
 
 
 class BottingHelpers:
     
+    @staticmethod
+    def botting_unrecoverable_issue(fsm: FSM) -> Generator[Any, Any, Any]:
+        if fsm is not None:
+            fsm.stop()
+        yield
+
+    @staticmethod
+    def inject_botting_behavior(fsm: FSM):
+        # Local imports to avoid circular import with resign_if_needed -> botting_helpers
+        from Widgets.CustomBehaviors.skills.botting.move_if_stuck import MoveIfStuckUtility
+        from Widgets.CustomBehaviors.skills.botting.move_to_distant_chest_if_path_exists import MoveToDistantChestIfPathExistsUtility
+        from Widgets.CustomBehaviors.skills.botting.move_to_enemy_if_close_enough import MoveToEnemyIfCloseEnoughUtility
+        from Widgets.CustomBehaviors.skills.botting.move_to_party_member_if_dead import MoveToPartyMemberIfDeadUtility
+        from Widgets.CustomBehaviors.skills.botting.move_to_party_member_if_in_aggro import MoveToPartyMemberIfInAggroUtility
+        from Widgets.CustomBehaviors.skills.botting.resign_if_needed import ResignIfNeededUtility
+        from Widgets.CustomBehaviors.skills.botting.wait_if_in_aggro import WaitIfInAggroUtility
+        from Widgets.CustomBehaviors.skills.botting.wait_if_lock_taken import WaitIfLockTakenUtility
+        from Widgets.CustomBehaviors.skills.botting.wait_if_party_member_mana_too_low import WaitIfPartyMemberManaTooLowUtility
+        from Widgets.CustomBehaviors.skills.botting.wait_if_party_member_needs_to_loot import WaitIfPartyMemberNeedsToLootUtility
+        from Widgets.CustomBehaviors.skills.botting.wait_if_party_member_too_far import WaitIfPartyMemberTooFarUtility
+
+        instance = CustomBehaviorLoader().custom_combat_behavior
+        if instance is None: raise Exception("CustomBehavior widget is required.")
+        # some are not finalized
+        # instance.inject_additionnal_utility_skills(MoveToDistantChestIfPathExistsUtility(instance.event_bus, instance.in_game_build))
+        instance.inject_additionnal_utility_skills(ResignIfNeededUtility(instance.event_bus, instance.in_game_build, on_failure= lambda: BottingHelpers.botting_unrecoverable_issue(fsm)))
+        instance.inject_additionnal_utility_skills(MoveToPartyMemberIfInAggroUtility(instance.event_bus, instance.in_game_build))
+        instance.inject_additionnal_utility_skills(MoveToEnemyIfCloseEnoughUtility(instance.event_bus, instance.in_game_build))
+        instance.inject_additionnal_utility_skills(MoveToPartyMemberIfDeadUtility(instance.event_bus, instance.in_game_build))
+        instance.inject_additionnal_utility_skills(WaitIfPartyMemberManaTooLowUtility(instance.event_bus, instance.in_game_build))
+        instance.inject_additionnal_utility_skills(WaitIfPartyMemberTooFarUtility(instance.event_bus, instance.in_game_build))
+        instance.inject_additionnal_utility_skills(WaitIfPartyMemberNeedsToLootUtility(instance.event_bus, instance.in_game_build))
+        instance.inject_additionnal_utility_skills(WaitIfInAggroUtility(instance.event_bus, instance.in_game_build))
+        instance.inject_additionnal_utility_skills(WaitIfLockTakenUtility(instance.event_bus, instance.in_game_build))
+
     @staticmethod
     def wrapper(action: Generator[Any, Any, bool], on_failure: Callable[[], Generator[Any, Any, Any]]) -> Generator[Any, Any, bool]:
         result: bool = yield from action
