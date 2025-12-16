1c70d8f7db12eb6fcf5484c096e824e28d0ff852
frenkey-derp <54779599+frenkey-derp@users.noreply.github.com>
2025-12-16 09:23:46 +0100

    Added docstrings to the new command system

--- HeroAI/commands.py
+++ HeroAI/commands.py
@@ -9,6 +9,20 @@ from Py4GWCoreLib.enums_src.Multiboxing_enums import SharedCommandType
 from Py4GWCoreLib.py4gwcorelib_src.Console import ConsoleLog
 
 class Command:
+    '''
+    Represents a command that can be executed and assigned to the HeroAI command system
+    Attributes:
+        name (str): The name of the command.
+        icon (str): The icon associated with the command.
+        command_function (Callable[[list[AccountData]], None] | None): The function to execute the command.
+        tooltip (str): The tooltip text for the command.
+        description (str): A detailed description of the command.
+        map_types (list[str]): The types of maps where the command is applicable.
+        
+    Methods:
+        is_separator() -> bool: Checks if the command is empty.
+        __call__(accounts: list[AccountData]): Executes the command function with the provided accounts.
+    '''
     def __init__(self, name: str, icon: str, command_function : Callable[[list[AccountData]], None] | None, tooltip : str = "", description: str = "", map_types: list[str] = ["Explorable", "Outpost"]) -> None:
         self.name = name
         self.icon = icon
@@ -22,12 +36,21 @@ class Command:
     def is_separator(self) -> bool:
         return self.name == "Empty" and self.command_function is None
     
-    ## make this class executable like a function
     def __call__(self, accounts: list[AccountData]):
         if self.command_function:
             self.command_function(accounts)
 
 class HeroAICommands:
+    '''
+    Singleton class that manages HeroAI commands.
+    Attributes:
+        Commands (dict[str, Command]): A dictionary of command names to Command objects.
+        Various predefined Command instances for different actions.
+        
+    Methods:
+        add_command(command: Command) -> bool: Adds a new command to the system.
+        remove_command(command: Command) -> bool: Removes a command from the system.
+    '''
     __instance = None
     __initialized = False
 
@@ -43,21 +66,19 @@ class HeroAICommands:
         self.__initialized = True
 
         self.Empty = Command("Empty", "", None)
-        self.PixelStack = Command("Pixel Stack", IconsFontAwesome5.ICON_COMPRESS_ARROWS_ALT, self.pixel_stack_command, "Pixel Stack Team")
-        self.InteractWithTarget = Command("Interact With Target", IconsFontAwesome5.ICON_HAND_POINT_RIGHT, self.interact_with_target_command, "Interact with current target")
-        self.UnlockChest = Command("Unlock Chest", IconsFontAwesome5.ICON_KEY, self.unlock_chest_command, "Unlock chest with current target")
-        self.TakeDialogWithTarget = Command("Dialog With Target", IconsFontAwesome5.ICON_COMMENT_DOTS, self.talk_and_dialog_with_target_command, "Take dialog with current target")
-        self.OpenConsumables = Command("Open Consumables", IconsFontAwesome5.ICON_CANDY_CANE, self.open_consumables_commands, "Open/Close Consumables Configuration Window")
-        self.FlagHeroes = Command("Flag Heroes", IconsFontAwesome5.ICON_FLAG, self.flag_heroes_command, "Flag all heroes", map_types=["Explorable"])
-        self.UnflagHeroes = Command("Unflag Heroes", IconsFontAwesome5.ICON_CIRCLE_XMARK, self.unflag_heroes_command, "Unflag all heroes", map_types=["Explorable"])
-        self.Resign = Command("Resign", IconsFontAwesome5.ICON_SKULL, self.resign_command, "Resign all accounts", map_types=["Explorable"])
-        self.DonateFaction = Command("Donate Faction", IconsFontAwesome5.ICON_DONATE, self.donate_faction_command, "Donate faction to guild")
-        self.GetStuck = Command("Get Stuck", IconsFontAwesome5.ICON_BUG, self.get_stuck_command, "Get stuck at a specific location")
-        # self.GetBlessing = Command("Get Blessing", IconsFontAwesome5.ICON_PRAYING_HANDS, self.get_blessing_command, "Get Blessing from nearby shrine")
-        self.PickUpLoot = Command("Pick up loot", IconsFontAwesome5.ICON_COINS, self.pick_up_loot_command, "Pick up loot from ground")
-        self.CombatPrep = Command("Prepare for Combat", IconsFontAwesome5.ICON_SHIELD_ALT, self.combat_prep_command, "Use Combat Preparations", map_types=["Explorable"])
-        self.DisbandParty = Command("Disband Party", IconsFontAwesome5.ICON_SIGN_OUT_ALT, self.leave_party_command, "Make all heroes leave party", map_types=["Outpost"])
-        self.FormParty = Command("Form Party", IconsFontAwesome5.ICON_USERS, self.invite_all_command, "Invite all heroes to party", map_types=["Outpost"])
+        self.PixelStack = Command("Pixel Stack", IconsFontAwesome5.ICON_COMPRESS_ARROWS_ALT, self.__pixel_stack_command, "Pixel Stack Team")
+        self.InteractWithTarget = Command("Interact With Target", IconsFontAwesome5.ICON_HAND_POINT_RIGHT, self.__interact_with_target_command, "Interact with current target")
+        self.UnlockChest = Command("Unlock Chest", IconsFontAwesome5.ICON_KEY, self.__unlock_chest_command, "Unlock chest with current target")
+        self.TakeDialogWithTarget = Command("Dialog With Target", IconsFontAwesome5.ICON_COMMENT_DOTS, self.__talk_and_dialog_with_target_command, "Take dialog with current target")
+        self.OpenConsumables = Command("Open Consumables", IconsFontAwesome5.ICON_CANDY_CANE, self.__open_consumables_commands, "Open/Close Consumables Configuration Window")
+        self.FlagHeroes = Command("Flag Heroes", IconsFontAwesome5.ICON_FLAG, self.__flag_heroes_command, "Flag all heroes", map_types=["Explorable"])
+        self.UnflagHeroes = Command("Unflag Heroes", IconsFontAwesome5.ICON_CIRCLE_XMARK, self.__unflag_heroes_command, "Unflag all heroes", map_types=["Explorable"])
+        self.Resign = Command("Resign", IconsFontAwesome5.ICON_SKULL, self.__resign_command, "Resign all accounts", map_types=["Explorable"])
+        self.DonateFaction = Command("Donate Faction", IconsFontAwesome5.ICON_DONATE, self.__donate_faction_command, "Donate faction to guild")
+        self.PickUpLoot = Command("Pick up loot", IconsFontAwesome5.ICON_COINS, self.__pick_up_loot_command, "Pick up loot from ground")
+        self.CombatPrep = Command("Prepare for Combat", IconsFontAwesome5.ICON_SHIELD_ALT, self.__combat_prep_command, "Use Combat Preparations", map_types=["Explorable"])
+        self.DisbandParty = Command("Disband Party", IconsFontAwesome5.ICON_SIGN_OUT_ALT, self.__leave_party_command, "Make all heroes leave party", map_types=["Outpost"])
+        self.FormParty = Command("Form Party", IconsFontAwesome5.ICON_USERS, self.__invite_all_command, "Invite all heroes to party", map_types=["Outpost"])
         
         self.__commands = [
             self.Empty,
@@ -70,9 +91,6 @@ class HeroAICommands:
             self.UnflagHeroes,
             self.Resign,
             self.DonateFaction,
-            # self.GetStuck,
-            
-            # self.GetBlessing,
             self.PickUpLoot,
             self.CombatPrep,
             self.DisbandParty,
@@ -80,10 +98,16 @@ class HeroAICommands:
         ]
     
     @property
-    def Commands(self) -> dict[str, Command]:      
+    def Commands(self) -> dict[str, Command]:   
+        '''Returns a dictionary of command names to Command objects.'''   
         return {cmd.name: cmd for cmd in self.__commands}
 
     def add_command(self, command: Command):
+        '''
+        Adds a new command to the system.
+        Args:
+            command (Command): The command to add.
+        '''
         if command not in self.__commands:
             self.__commands.append(command)
             return True
@@ -91,60 +115,55 @@ class HeroAICommands:
         return False
     
     def remove_command(self, command: Command):
+        '''
+        Removes a command from the system.
+        Args:
+            command (Command): The command to remove.
+        '''
         if command in self.__commands:
             self.__commands.remove(command)
             return True
         
         return False
     
-    def get_stuck_command(self, accounts: list[AccountData]):
-        x,y = 8730, 7402
-        GLOBAL_CACHE.ShMem.SendMessage(
-            GLOBAL_CACHE.Player.GetAccountEmail(),
-            GLOBAL_CACHE.Player.GetAccountEmail(),
-            SharedCommandType.PixelStack,
-            (x, y, 0, 0)
-        )
+    def send_dialog(self, accounts: list[AccountData], dialog_option: int):
+        sender_email = GLOBAL_CACHE.Player.GetAccountEmail()
+        own_map_id = GLOBAL_CACHE.Map.GetMapID()
+        own_region = GLOBAL_CACHE.Map.GetRegion()[0]
+        own_district = GLOBAL_CACHE.Map.GetDistrict()
+        own_language = GLOBAL_CACHE.Map.GetLanguage()[0]
         
-        x,y = -1062, 1021
-        GLOBAL_CACHE.ShMem.SendMessage(
-            GLOBAL_CACHE.Player.GetAccountEmail(),
-            GLOBAL_CACHE.Player.GetAccountEmail(),
-            SharedCommandType.PixelStack,
-            (x, y, 0, 0)
-        )
-    
-    def leave_party_command(self, accounts: list[AccountData]):
+        for account in accounts:
+            same_map = own_map_id == account.MapID and own_region == account.MapRegion and own_district == account.MapDistrict and own_language == account.MapLanguage
+            
+            if same_map:
+                GLOBAL_CACHE.ShMem.SendMessage(sender_email, account.AccountEmail, SharedCommandType.SendDialog, (dialog_option, 0, 0, 0))
+                
+    def __leave_party_command(self, accounts: list[AccountData]):
         sender_email = GLOBAL_CACHE.Player.GetAccountEmail()        
         
         for account in accounts:
             GLOBAL_CACHE.ShMem.SendMessage(sender_email, account.AccountEmail, SharedCommandType.LeaveParty, (0, 0, 0, 0))
     
-    def combat_prep_command(self, accounts: list[AccountData]):
+    def __combat_prep_command(self, accounts: list[AccountData]):
         sender_email = GLOBAL_CACHE.Player.GetAccountEmail()        
         
         for account in accounts:
             GLOBAL_CACHE.ShMem.SendMessage(sender_email, account.AccountEmail, SharedCommandType.UseSkillCombatPrep, (0, 0, 0, 0))
     
-    def pick_up_loot_command(self, accounts: list[AccountData]):
+    def __pick_up_loot_command(self, accounts: list[AccountData]):
         sender_email = GLOBAL_CACHE.Player.GetAccountEmail()        
         
         for account in accounts:
             GLOBAL_CACHE.ShMem.SendMessage(sender_email, account.AccountEmail, SharedCommandType.PickUpLoot, (0, 0, 0, 0))
-    
-    def get_blessing_command(self, accounts: list[AccountData]):
-        sender_email = GLOBAL_CACHE.Player.GetAccountEmail()        
         
-        for account in accounts:
-            GLOBAL_CACHE.ShMem.SendMessage(sender_email, account.AccountEmail, SharedCommandType.GetBlessing, (0, 0, 0, 0))
-    
-    def donate_faction_command(self, accounts: list[AccountData]):
+    def __donate_faction_command(self, accounts: list[AccountData]):
         sender_email = GLOBAL_CACHE.Player.GetAccountEmail()
         
         for account in accounts:
             GLOBAL_CACHE.ShMem.SendMessage(sender_email, account.AccountEmail, SharedCommandType.DonateToGuild, (0, 0, 0, 0))
     
-    def invite_all_command(self, accounts: list[AccountData]):
+    def __invite_all_command(self, accounts: list[AccountData]):
         sender_email = GLOBAL_CACHE.Player.GetAccountEmail()
         sender_id = GLOBAL_CACHE.Player.GetAgentID()
         
@@ -181,15 +200,14 @@ class HeroAICommands:
                     GLOBAL_CACHE.Map.GetLanguage()[0],
                 )
             ) 
-        
-    
-    def resign_command(self, accounts: list[AccountData]):
+            
+    def __resign_command(self, accounts: list[AccountData]):
         sender_email = GLOBAL_CACHE.Player.GetAccountEmail()
         
         for account in accounts:
             GLOBAL_CACHE.ShMem.SendMessage(sender_email, account.AccountEmail, SharedCommandType.Resign, (0, 0, 0, 0))
         
-    def pixel_stack_command(self, accounts: list[AccountData]):
+    def __pixel_stack_command(self, accounts: list[AccountData]):
         player_x, player_y = GLOBAL_CACHE.Player.GetXY()
         sender_email = GLOBAL_CACHE.Player.GetAccountEmail()
         
@@ -199,7 +217,7 @@ class HeroAICommands:
             
             GLOBAL_CACHE.ShMem.SendMessage(sender_email, account.AccountEmail, SharedCommandType.PixelStack, (player_x, player_y, 0, 0))
             
-    def unlock_chest_command(self, accounts: list[AccountData]):
+    def __unlock_chest_command(self, accounts: list[AccountData]):
         sender_email = GLOBAL_CACHE.Player.GetAccountEmail()        
         target_id = GLOBAL_CACHE.Player.GetTargetID()
         
@@ -227,44 +245,31 @@ class HeroAICommands:
         
         GLOBAL_CACHE.ShMem.SendMessage(sender_email, lowest_party_index_account.AccountEmail, SharedCommandType.OpenChest, (target_id, 1, 0, 0))
             
-    def interact_with_target_command(self, accounts: list[AccountData]):
+    def __interact_with_target_command(self, accounts: list[AccountData]):
         sender_email = GLOBAL_CACHE.Player.GetAccountEmail()        
         target_id = GLOBAL_CACHE.Player.GetTargetID()
         
         for account in accounts:
             GLOBAL_CACHE.ShMem.SendMessage(sender_email, account.AccountEmail, SharedCommandType.InteractWithTarget, (target_id, 0, 0, 0))
     
-    def talk_and_dialog_with_target_command(self, accounts: list[AccountData]):
+    def __talk_and_dialog_with_target_command(self, accounts: list[AccountData]):
         sender_email = GLOBAL_CACHE.Player.GetAccountEmail()        
         target_id = GLOBAL_CACHE.Player.GetTargetID()
         
         for account in accounts:
             GLOBAL_CACHE.ShMem.SendMessage(sender_email, account.AccountEmail, SharedCommandType.TakeDialogWithTarget, (target_id, 1, 0, 0))
 
-    def send_dialog(self, accounts: list[AccountData], dialog_option: int):
-        sender_email = GLOBAL_CACHE.Player.GetAccountEmail()
-        own_map_id = GLOBAL_CACHE.Map.GetMapID()
-        own_region = GLOBAL_CACHE.Map.GetRegion()[0]
-        own_district = GLOBAL_CACHE.Map.GetDistrict()
-        own_language = GLOBAL_CACHE.Map.GetLanguage()[0]
-        
-        for account in accounts:
-            same_map = own_map_id == account.MapID and own_region == account.MapRegion and own_district == account.MapDistrict and own_language == account.MapLanguage
-            
-            if same_map:
-                GLOBAL_CACHE.ShMem.SendMessage(sender_email, account.AccountEmail, SharedCommandType.SendDialog, (dialog_option, 0, 0, 0))
-
-    def open_consumables_commands(self, accounts: list[AccountData]):
+    def __open_consumables_commands(self, accounts: list[AccountData]):
         from HeroAI import ui
         ui.show_configure_consumables_window()
 
-    def flag_heroes_command(self, accounts: list[AccountData]):
+    def __flag_heroes_command(self, accounts: list[AccountData]):
         from HeroAI import ui, windows
         windows.capture_flag_all = True
         windows.capture_hero_flag = True
         windows.capture_hero_index = 0
         windows.one_time_set_flag = False      
     
-    def unflag_heroes_command(self, accounts: list[AccountData]):
+    def __unflag_heroes_command(self, accounts: list[AccountData]):
         from HeroAI import ui, windows
         windows.ClearFlags = True
