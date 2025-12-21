5653051f5e880c438767567f5d3207ed8b2965f1
frenkey-derp <54779599+frenkey-derp@users.noreply.github.com>
2025-12-21 21:07:05 +0100

    Fixes and Layout Helpers added

--- Py4GWCoreLib/ImGui_src/types.py
+++ Py4GWCoreLib/ImGui_src/types.py
@@ -17,17 +17,79 @@ class StyleTheme(IntEnum):
     Contrast = 4
     Negative = 5
     
-class Alignment(IntEnum):
-    TopLeft = 0
-    TopCenter = 1
-    TopRight = 2
-    MidLeft = 3
-    MidCenter = 4
-    MidRight = 5
-    BottomLeft = 6
-    BottomCenter = 7
-    BottomRight = 8
+class VerticalAlignment(IntEnum):
+    Above = 0
+    Top = 1
+    Middle = 2
+    Bottom = 3
+    Below = 4
 
+
+class HorizontalAlignment(IntEnum):
+    LeftOf = 0
+    Left = 1
+    Center = 2
+    Right = 3
+    RightOf = 4
+
+
+_H_SHIFT = 0
+_V_SHIFT = 3
+
+_H_MASK = 0b111 << _H_SHIFT
+_V_MASK = 0b111 << _V_SHIFT
+
+class Alignment(IntEnum):
+    # bit layout:
+    # bits 0–2 : horizontal (0–4)
+    # bits 3–5 : vertical   (0–4)
+
+    AboveLeftOf    = (VerticalAlignment.Above  << _V_SHIFT) | HorizontalAlignment.LeftOf
+    AboveLeft      = (VerticalAlignment.Above  << _V_SHIFT) | HorizontalAlignment.Left
+    AboveCenter    = (VerticalAlignment.Above  << _V_SHIFT) | HorizontalAlignment.Center
+    AboveRight     = (VerticalAlignment.Above  << _V_SHIFT) | HorizontalAlignment.Right
+    AboveRightOf   = (VerticalAlignment.Above  << _V_SHIFT) | HorizontalAlignment.RightOf
+
+    TopLeftOf      = (VerticalAlignment.Top    << _V_SHIFT) | HorizontalAlignment.LeftOf
+    TopLeft        = (VerticalAlignment.Top    << _V_SHIFT) | HorizontalAlignment.Left
+    TopCenter      = (VerticalAlignment.Top    << _V_SHIFT) | HorizontalAlignment.Center
+    TopRight       = (VerticalAlignment.Top    << _V_SHIFT) | HorizontalAlignment.Right
+    TopRightOf     = (VerticalAlignment.Top    << _V_SHIFT) | HorizontalAlignment.RightOf
+
+    MidLeftOf      = (VerticalAlignment.Middle << _V_SHIFT) | HorizontalAlignment.LeftOf
+    MidLeft        = (VerticalAlignment.Middle << _V_SHIFT) | HorizontalAlignment.Left
+    MidCenter      = (VerticalAlignment.Middle << _V_SHIFT) | HorizontalAlignment.Center
+    MidRight       = (VerticalAlignment.Middle << _V_SHIFT) | HorizontalAlignment.Right
+    MidRightOf     = (VerticalAlignment.Middle << _V_SHIFT) | HorizontalAlignment.RightOf
+
+    BottomLeftOf   = (VerticalAlignment.Bottom << _V_SHIFT) | HorizontalAlignment.LeftOf
+    BottomLeft     = (VerticalAlignment.Bottom << _V_SHIFT) | HorizontalAlignment.Left
+    BottomCenter   = (VerticalAlignment.Bottom << _V_SHIFT) | HorizontalAlignment.Center
+    BottomRight    = (VerticalAlignment.Bottom << _V_SHIFT) | HorizontalAlignment.Right
+    BottomRightOf  = (VerticalAlignment.Bottom << _V_SHIFT) | HorizontalAlignment.RightOf
+
+    BelowLeftOf    = (VerticalAlignment.Below  << _V_SHIFT) | HorizontalAlignment.LeftOf
+    BelowLeft      = (VerticalAlignment.Below  << _V_SHIFT) | HorizontalAlignment.Left
+    BelowCenter    = (VerticalAlignment.Below  << _V_SHIFT) | HorizontalAlignment.Center
+    BelowRight     = (VerticalAlignment.Below  << _V_SHIFT) | HorizontalAlignment.Right
+    BelowRightOf   = (VerticalAlignment.Below  << _V_SHIFT) | HorizontalAlignment.RightOf
+
+    @property
+    def vertical(self) -> VerticalAlignment:
+        return VerticalAlignment((self.value & _V_MASK) >> _V_SHIFT)
+
+    @property
+    def horizontal(self) -> HorizontalAlignment:
+        return HorizontalAlignment((self.value & _H_MASK) >> _H_SHIFT)
+
+    @classmethod
+    def from_parts(
+        cls,
+        vertical: VerticalAlignment,
+        horizontal: HorizontalAlignment,
+    ) -> "Alignment":
+        return cls((vertical << _V_SHIFT) | horizontal)
+    
 class TextDecorator(IntEnum):
     None_ = 0
     Underline = 1
