6ef5b5bd6198f1b949c23357e63f96d7bee923b9
frenkey-derp <54779599+frenkey-derp@users.noreply.github.com>
2026-01-23 19:57:21 +0100

    Update Messaging.py

--- Widgets/Messaging.py
+++ Widgets/Messaging.py
@@ -1363,12 +1363,9 @@ def TravelToGuildHall(index, message):
 # region ProcessMessages
 def ProcessMessages():
     account_email = Player.GetAccountEmail()
-    index, message = GLOBAL_CACHE.ShMem.PreviewNextMessage(account_email, include_running=True)
+    index, message = GLOBAL_CACHE.ShMem.GetNextMessage(account_email)
 
     if index == -1 or message is None:
-        return
-    
-    if message.Running:
         return
 
     match message.Command:
